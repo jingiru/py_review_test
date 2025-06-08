2cf0bd861f0def25788c8f817ed4dfe712725468
jingiru <35403685+jingiru@users.noreply.github.com>
2025-06-09 01:59:28 +0900

    Add 30 additional Python quiz problems

--- app.py
+++ app.py
@@ -26,6 +26,126 @@ questions = [
     {
         "code": 'print("1\\n2\\n3")',
         "output": "1\n2\n3"
+    },
+    {
+        "code": 'print(len("Python"))',
+        "output": "6"
+    },
+    {
+        "code": 'print(2 * 3 + 4)',
+        "output": "10"
+    },
+    {
+        "code": 'x = [1, 2, 3]\nprint(x[1])',
+        "output": "2"
+    },
+    {
+        "code": 'print("Py" + "thon")',
+        "output": "Python"
+    },
+    {
+        "code": 'for i in range(1, 4):\n    print("A" * i)',
+        "output": "A\nAA\nAAA"
+    },
+    {
+        "code": 'print(sum([1, 2, 3]))',
+        "output": "6"
+    },
+    {
+        "code": 'print("Hello".lower())',
+        "output": "hello"
+    },
+    {
+        "code": 'print(10 // 3)',
+        "output": "3"
+    },
+    {
+        "code": 'print(10 % 3)',
+        "output": "1"
+    },
+    {
+        "code": 'print([i for i in range(3)])',
+        "output": "[0, 1, 2]"
+    },
+    {
+        "code": 'print(",".join(["a", "b", "c"]))',
+        "output": "a,b,c"
+    },
+    {
+        "code": 'print(min(5, 7, 2))',
+        "output": "2"
+    },
+    {
+        "code": 'print(max([1, 5, 3]))',
+        "output": "5"
+    },
+    {
+        "code": 'print(type(3.5))',
+        "output": "<class 'float'>"
+    },
+    {
+        "code": 'print(len([1, [2, 3], 4]))',
+        "output": "3"
+    },
+    {
+        "code": 'd = {"a": 1}\nprint(d.get("a"))',
+        "output": "1"
+    },
+    {
+        "code": 'print(sorted([3, 1, 2]))',
+        "output": "[1, 2, 3]"
+    },
+    {
+        "code": 'print(",".join(map(str, [1, 2, 3])))',
+        "output": "1,2,3"
+    },
+    {
+        "code": 'for i in range(3):\n    print(i**2)',
+        "output": "0\n1\n4"
+    },
+    {
+        "code": 'print(list(range(1, 4)))',
+        "output": "[1, 2, 3]"
+    },
+    {
+        "code": 'print(5 > 3)',
+        "output": "True"
+    },
+    {
+        "code": 'print(bool(""))',
+        "output": "False"
+    },
+    {
+        "code": 'print("{:.2f}".format(3.14159))',
+        "output": "3.14"
+    },
+    {
+        "code": 'print("Py\\\"thon")',
+        "output": "Py\"thon"
+    },
+    {
+        "code": 'print(len(set([1, 1, 2])))',
+        "output": "2"
+    },
+    {
+        "code": 'print("Hello World".split()[1])',
+        "output": "World"
+    },
+    {
+        "code": 'x = [1, 2, 3]\nx.append(4)\nprint(len(x))',
+        "output": "4"
+    },
+    {
+        "code": 'print("-".join(["2021", "09", "01"]))',
+        "output": "2021-09-01"
+    },
+    {
+        "code": 'print((lambda x: x*2)(3))',
+        "output": "6"
+    },
+    {
+        "code": 'for char in "Hi":\n    print(char)',
+        "output": "H\ni"
     }
 ]
 
