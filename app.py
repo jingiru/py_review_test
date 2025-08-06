c40b2f97fcea9a939b300ffd108076180ccb4a3b
jingiru <skyjjw79@hanmail.net>
2025-08-06 17:45:27 +0900

    어려운 문제 다 뺌

--- app.py
+++ app.py
@@ -64,10 +64,6 @@ questions = [
         "output": "6"
     },
     {
-        "code": 'print("Hello".lower())',
-        "output": "hello"
-    },
-    {
         "code": 'print(10 // 3)',
         "output": "3"
     },
@@ -80,10 +76,6 @@ questions = [
         "output": "[0, 1, 2]"
     },
     {
-        "code": 'print(",".join(["a", "b", "c"]))',
-        "output": "a,b,c"
-    },
-    {
         "code": 'print(min(5, 7, 2))',
         "output": "2"
     },
@@ -92,72 +84,8 @@ questions = [
         "output": "5"
     },
     {
-        "code": 'print(type(3.5))',
-        "output": "<class 'float'>"
-    },
-    {
-        "code": 'print(len([1, [2, 3], 4]))',
-        "output": "3"
-    },
-    {
-        "code": 'd = {"a": 1}\nprint(d.get("a"))',
-        "output": "1"
-    },
-    {
-        "code": 'print(sorted([3, 1, 2]))',
-        "output": "[1, 2, 3]"
-    },
-    {
-        "code": 'print(",".join(map(str, [1, 2, 3])))',
-        "output": "1,2,3"
-    },
-    {
         "code": 'for i in range(3):\n    print(i**2)',
         "output": "0\n1\n4"
-    },
-    {
-        "code": 'print(list(range(1, 4)))',
-        "output": "[1, 2, 3]"
-    },
-    {
-        "code": 'print(5 > 3)',
-        "output": "True"
-    },
-    {
-        "code": 'print(bool(""))',
-        "output": "False"
-    },
-    {
-        "code": 'print("{:.2f}".format(3.14159))',
-        "output": "3.14"
-    },
-    {
-        "code": 'print("Py\\\"thon")',
-        "output": "Py\"thon"
-    },
-    {
-        "code": 'print(len(set([1, 1, 2])))',
-        "output": "2"
-    },
-    {
-        "code": 'print("Hello World".split()[1])',
-        "output": "World"
-    },
-    {
-        "code": 'x = [1, 2, 3]\nx.append(4)\nprint(len(x))',
-        "output": "4"
-    },
-    {
-        "code": 'print("-".join(["2021", "09", "01"]))',
-        "output": "2021-09-01"
-    },
-    {
-        "code": 'print((lambda x: x*2)(3))',
-        "output": "6"
-    },
-    {
-        "code": 'for char in "Hi":\n    print(char)',
-        "output": "H\ni"
     }
 ]
 
@@ -178,7 +106,7 @@ def next_question():
         "code": problem["code"],
         "output": problem["output"]
     }
-    
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
